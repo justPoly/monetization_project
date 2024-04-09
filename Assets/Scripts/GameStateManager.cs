3f9e45a2e67fe91b0a98bda12dba12534276b3ba
Polycarp Atalor <polycarpatalor@gmail.com>
2024-04-09 21:16:17 +0100

    Updated project files

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Managers/GameState Manager")]
+#if UNITY_EDITOR
+[FilePath("Scriptable Objects/Managers/GameStateManager.asset", FilePathAttribute.Location.PreferencesFolder)]
+#endif
+public class GameStateManager : SingletonScriptableObject<GameStateManager>
+{
+    [FancyHeader("GAMESTATE MANAGER", 3f, "#D4AF37", 8.5f, order = 0)]
+    [Space(order = 1)]
+    [CustomProgressBar(hideWhenZero = true, label = "m_loadingTxt"), SerializeField] public float m_loadingBar;
+    [HideInInspector] public string m_loadingTxt;
+    [HideInInspector] public bool m_loadingDone = false;
+
+    [SerializeField] private ApplicationManager m_applicationManager;
+    private ApplicationManager m_saveApplicationManager;
+
+
+    public static ApplicationManager ApplicationManager
+    {
+        get { return Instance.m_applicationManager; }
+
+    }
+
+    public void Init()
+    {
+        
+    }
+
+}
