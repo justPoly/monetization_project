f1d7856135182fbb264bcb4c008e0597176b4fa3
Polycarp Atalor <polycarpatalor@gmail.com>
2024-06-19 08:49:28 +0200

    Added firebase events to track ad performance and set privacy and term of use button

--- Assets/Scripts/Unity Ads/BannerAds.cs
+++ Assets/Scripts/Unity Ads/BannerAds.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
+using Firebase.Analytics;
 
 public class BannerAds : MonoBehaviour
 {
@@ -14,9 +14,9 @@ public class BannerAds : MonoBehaviour
     private void Awake()
     {
         #if UNITY_IOS
-                adUnitId = iosAdUnitId;
+            adUnitId = iosAdUnitId;
         #elif UNITY_ANDROID
-                adUnitId = androidAdUnitId;
+            adUnitId = androidAdUnitId;
         #endif
         
         Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
@@ -33,8 +33,8 @@ public class BannerAds : MonoBehaviour
             };
 
             Advertisement.Banner.Load(adUnitId, options);
+            FirebaseAnalytics.LogEvent("banner_ad_load_attempt", "ad_unit_id", adUnitId);
         }
-        
     }
 
     public void ShowBannerAd()
@@ -48,32 +48,50 @@ public class BannerAds : MonoBehaviour
                 hideCallback = BannerHidden
             };
             Advertisement.Banner.Show(adUnitId, options);
+            FirebaseAnalytics.LogEvent("banner_ad_show_attempt", "ad_unit_id", adUnitId);
         }
     }
 
     public void HideBannerAd()
     {
         Advertisement.Banner.Hide();
+        FirebaseAnalytics.LogEvent("banner_ad_hidden", "ad_unit_id", adUnitId);
     }
 
-
     #region Show Callbacks
-    private void BannerHidden()   {    }
+    private void BannerHidden()
+    {
+        Debug.Log("Banner Ad Hidden");
+        FirebaseAnalytics.LogEvent("banner_ad_hidden", "ad_unit_id", adUnitId);
+    }
 
-    private void BannerClicked()   {    }
+    private void BannerClicked()
+    {
+        Debug.Log("Banner Ad Clicked");
+        FirebaseAnalytics.LogEvent("banner_ad_clicked", "ad_unit_id", adUnitId);
+    }
 
-    private void BannerShown()    {    }
+    private void BannerShown()
+    {
+        Debug.Log("Banner Ad Shown");
+        FirebaseAnalytics.LogEvent("banner_ad_shown", "ad_unit_id", adUnitId);
+    }
     #endregion
 
     #region Load Callbacks
-    private void BannerLoadedError(string message)    {    }
+    private void BannerLoadedError(string message)
+    {
+        Debug.LogError("Banner Ad Load Failed: " + message);
+        FirebaseAnalytics.LogEvent("banner_ad_load_failed", new Parameter[] {
+            new Parameter("ad_unit_id", adUnitId),
+            new Parameter("error_message", message)
+        });
+    }
 
     private void BannerLoaded()
     {
         Debug.Log("Banner Ad Loaded");
+        FirebaseAnalytics.LogEvent("banner_ad_loaded", "ad_unit_id", adUnitId);
     }
     #endregion
-
-
-
 }
